59d5a04e298536ebf8fa85aef675af0bcef6df9f
Victor Deng <victor-deng@qq.com>
2017-09-17 16:59:45 +0800

    Initial commit

---
+++ MyDemoPractice/MyDemo1/MainForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace MyDemo1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.新建窗体ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.层叠ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.水平ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.垂直ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guangToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.新建窗体ToolStripMenuItem,
+            this.层叠ToolStripMenuItem,
+            this.水平ToolStripMenuItem,
+            this.垂直ToolStripMenuItem,
+            this.guangToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 新建窗体ToolStripMenuItem
+            // 
+            this.新建窗体ToolStripMenuItem.Name = "新建窗体ToolStripMenuItem";
+            this.新建窗体ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.新建窗体ToolStripMenuItem.Text = "新建窗体";
+            this.新建窗体ToolStripMenuItem.Click += new System.EventHandler(this.新建窗体ToolStripMenuItem_Click);
+            // 
+            // 层叠ToolStripMenuItem
+            // 
+            this.层叠ToolStripMenuItem.Name = "层叠ToolStripMenuItem";
+            this.层叠ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.层叠ToolStripMenuItem.Text = "层叠";
+            this.层叠ToolStripMenuItem.Click += new System.EventHandler(this.层叠ToolStripMenuItem_Click);
+            // 
+            // 水平ToolStripMenuItem
+            // 
+            this.水平ToolStripMenuItem.Name = "水平ToolStripMenuItem";
+            this.水平ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.水平ToolStripMenuItem.Text = "水平";
+            this.水平ToolStripMenuItem.Click += new System.EventHandler(this.水平ToolStripMenuItem_Click);
+            // 
+            // 垂直ToolStripMenuItem
+            // 
+            this.垂直ToolStripMenuItem.Name = "垂直ToolStripMenuItem";
+            this.垂直ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.垂直ToolStripMenuItem.Text = "垂直";
+            this.垂直ToolStripMenuItem.Click += new System.EventHandler(this.垂直ToolStripMenuItem_Click);
+            // 
+            // guangToolStripMenuItem
+            // 
+            this.guangToolStripMenuItem.Name = "guangToolStripMenuItem";
+            this.guangToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.guangToolStripMenuItem.Text = "关闭";
+            this.guangToolStripMenuItem.Click += new System.EventHandler(this.guangToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 623);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "MDI窗体";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 新建窗体ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 层叠ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 水平ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 垂直ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guangToolStripMenuItem;
+    }
+}
---
+++ MyDemoPractice/MyDemo4/MainForm.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace MyDemo4
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(215, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "登录";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(215, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(211, 25);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(215, 120);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(211, 25);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(146, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "用户名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(146, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "密码";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(220, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(206, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "====以下是你的登录信息====";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(143, 268);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "用户名";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(143, 312);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "密码";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(213, 268);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 15);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "等待输入";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(213, 312);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 15);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "等待输入";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 455);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "登录界面";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
